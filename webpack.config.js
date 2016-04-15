5841f098c310cfadee5743123e63b2fa37e5da1b
Robin Millette <robin@millette.info>
2016-04-15 16:54:36 -0400

    Responsive type.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-    entry: './entry.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    devServer: {
-        inline: true,
-        port: 1234
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader!postcss-loader'
-            }
-        ]
-    },
-    postcss: [
-        require('lost'),
-        require('autoprefixer')
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  devServer: {
+    inline: true,
+    port: 1234
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      }
     ]
+  },
+  postcss: [
+    require('postcss-responsive-type')(),
+    require('lost'),
+    require('autoprefixer')
+  ]
 }
